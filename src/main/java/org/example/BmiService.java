bd749795c41606365ad00656a7c86688bc2ed01d
79911 <Petrovlistov@gmail.com>
2023-07-28 23:52:05 +0300

    changes

---
+++ src/main/java/org/example/BmiService.java
@@ -0,0 +1,10 @@
+package org.example;
+class BmiService { private final double weight;
+    private final double height;
+    public BmiService(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    } public double calculate() {
+        return weight / (Math.pow(height, 2));
+    }
+}
