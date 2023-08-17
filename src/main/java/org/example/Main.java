abc7cab94de85e0358946ceb2417dc185e9d51f2
79911 <Petrovlistov@gmail.com>
2023-08-18 00:17:04 +0300

    changes

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        BmiService bmiService = new BmiService(98, 1.87);
-        int result = (int) bmiService.calculate();
-        System.out.println("Индекс массы тела:" + result);
-    }
-}
