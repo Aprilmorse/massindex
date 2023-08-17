60c413bca207c7d27547447a064232cb70cef5d8
79911 <Petrovlistov@gmail.com>
2023-08-18 02:22:25 +0300

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        org.example.BmiService bmiService = new org.example.BmiService (98, 1.87);
+        org.example.BmiService bmiService = new org.example.BmiService(98, 1.87);
         int result = (int) bmiService.calculate();
         System.out.println("Индекс массы тела:" + result);
     }
